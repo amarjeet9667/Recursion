7c12e27226154b13d502127034d57bd1a744b7de
amarjeet9667 <jeetstark1@gmail.com>
2025-11-11 23:35:09 +0530

    star pattren

---
+++ loginApi/java_practice_code/BinaryTreeYT.java
@@ -0,0 +1,36 @@
+public class BinaryTreeYT {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildNodes(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildNodes(nodes);
+            newNode.right = buildNodes(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] arg){
+        int nodes [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildNodes(nodes);
+        System.out.println(root.data);
+    }
+}
--- loginApi/java_practice_code/Patterns.java
+++ loginApi/java_practice_code/Patterns.java
@@ -21,23 +21,63 @@ public class Patterns {
     // }
 
     // Void ractangle
-    public static void main(String[] arg){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Row:- ");
-        int row = sc.nextInt();
-        System.out.print("Enter Column:- ");
-        int col = sc.nextInt();
+    // public static void main(String[] arg){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter Row:- ");
+    //     int row = sc.nextInt();
+    //     System.out.print("Enter Column:- ");
+    //     int col = sc.nextInt();
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(i == 0 || j == 0 || i == row-1 || j == col-1) {
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
+    //     for(int i = 0; i < row; i++){
+    //         for(int j = 0; j < col; j++){
+    //             if(i == 0 || j == 0 || i == row-1 || j == col-1) {
+    //                 System.out.print("*");
+    //             }else{
+    //                 System.out.print(" ");
+    //             }
                
-            }
+    //         }
 
+    //         System.out.println();
+    //     }
+    // }
+
+    // Start stair pattern
+    // public static void main(String[] arg){
+        
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter number of rows:- ");
+    //     int row = sc.nextInt();
+        
+
+    //     for(int i = 0; i < row; i ++){
+
+    //         for(int j = 0; j < i+1; j++){
+                
+    //                 System.out.print("*");
+                
+
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    // Reverse pattern
+    public static void main(String[] arg){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows:- ");
+        int row = sc.nextInt();
+        
+
+        for(int i = row; i > 0; i --){
+
+            for(int j = 0; j < i; j++){
+                
+                    System.out.print("*");
+                
+
+            }
             System.out.println();
         }
     }
